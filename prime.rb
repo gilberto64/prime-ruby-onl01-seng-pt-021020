fc064830f096dc593ccbe1e247be46955e8f22fa
Gilberto Ocampo <ocampo@gate.net>
2020-02-23 13:01:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime(arr)
+
+counter = 0 
+
+while counter < arr.length 
+  if arr[counter].integer! = false
+  elseif arr[counter] < 2 = false
+  elseif arr[counter] == 2 = true
+  elseif arr[counter] even! = false 
+  elseif square root of arr[counter] is an integer = false 
+  elseif arr[counter] / by (3,5,7,11,13) = integer = false     
+  else true  
+  end
+  counter +=1
+end 
+end 
